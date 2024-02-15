0f17cc8cd5a8e5f1653beb3fcaa0d0ebfc0bc281
Sneha Parkar <snehaparkar007@gmail.com>
2024-02-15 13:46:20 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.ffac6ae876d2a974b02ddb15c151f777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d59417b1296f7d090e88c11118ac8e",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "2ba963b74e3c4949dd9b",
+    "url": "/note-app/static/css/main.2926f41f.chunk.css"
+  },
+  {
+    "revision": "21e8d4813ba2a467248e",
+    "url": "/note-app/static/js/2.839cefa3.chunk.js"
+  },
+  {
+    "revision": "ba24e68c30683027ff4d781ec4360f79",
+    "url": "/note-app/static/js/2.839cefa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba963b74e3c4949dd9b",
+    "url": "/note-app/static/js/main.bb757b9e.chunk.js"
+  },
+  {
+    "revision": "46211b81bc79b455c5d0",
+    "url": "/note-app/static/js/runtime-main.387be6cf.js"
+  }
+]);
